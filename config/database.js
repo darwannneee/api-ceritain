8a70d741449b7e4c1d0e6884422ece3abb9cd4a7
darwannneee <darwanchannel1@gmail.com>
2023-07-16 21:01:45 +0700

    Fix mysql2

--- config/database.js
+++ config/database.js
@@ -1,10 +1,14 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize"
+import mysql2 from 'mysql2'; // Needed to fix sequelize issues with WebPack;
 
 const db = new Sequelize('ceritain', 'Ceritain', 'LombaDb@3', {
     host: 'ceritain.mysql.database.azure.com',
     dialect: "mysql", 
     define: {
-        timestamps: false
+        timestamps: false,
+        dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT
     }
 })
 
